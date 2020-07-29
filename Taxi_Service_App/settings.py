8e7e1648e59dfd53720ffb3f69d3a873a28430a0
leenqa <201706172@bethlehem.edu>
2020-07-29 09:52:04 +0300

    updated tests

--- Taxi_Service_App/settings.py
+++ Taxi_Service_App/settings.py
@@ -106,23 +106,23 @@ WSGI_APPLICATION = 'Taxi_Service_App.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 ALLOWED_HOSTS = ['127.0.0.1']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['MYSQL_DATABASE'],
-        'USER':  os.environ['MYSQL_USER'],
-        'PASSWORD': os.environ['MYSQL_PASSWORD'],
-        'HOST': 'db',
-        'PORT': 3306,
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ['MYSQL_DATABASE'],
+#         'USER':  os.environ['MYSQL_USER'],
+#         'PASSWORD': os.environ['MYSQL_PASSWORD'],
+#         'HOST': 'db',
+#         'PORT': 3306,
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
