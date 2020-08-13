7b11699aa219bd4888713f2b3a1dccf83f69aed4
leen Anwar <Leen@exalt.ps>
2020-08-13 14:37:28 +0300

    last commit

--- Taxi_Service_App/settings.py
+++ Taxi_Service_App/settings.py
@@ -107,23 +107,23 @@ WSGI_APPLICATION = 'Taxi_Service_App.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'docker',
-        'USER': 'root',
-        'PASSWORD': 'docker',
-        'HOST': 'db',
-        'PORT': 3306,
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'docker',
+#         'USER': 'root',
+#         'PASSWORD': 'docker',
+#         'HOST': 'db',
+#         'PORT': 3306,
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
